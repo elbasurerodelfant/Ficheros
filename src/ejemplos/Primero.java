f4d2fa13c6d82727689104488a67cf36c0a580a5
Javier Moran <javiermoran7@gmail.com>
2019-04-03 12:46:04 +0200

    ddddddd

--- src/ejemplos/Primero.java
+++ src/ejemplos/Primero.java
@@ -9,22 +9,19 @@ public class Primero {
 
 	public static void main(String[] args) {
 		DataOutputStream out = null;
-		try{
-			out = new DataOutputStream (new FileOutputStream("C:\\Users\\aula9\\Desktop"));
-			out.writeInt(43291);
-			out.writeLong(5564444444444423565233);
-			out.writeBoolean(true);
-			out.writeBoolean(false);
-		}catch(FileNotFoundException e) {
+		try {
+			out = new DataOutputStream(new FileOutputStream("C:\\Users\\aula9\\Desktop"));
+
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} finally {
 			if (out != null)
-				try{
+				try {
 					out.close();
-				} catch(IOException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 		}
 	}
-	
+
 }
--- src/ejemplos/Segundo.java
+++ src/ejemplos/Segundo.java
@@ -11,21 +11,19 @@ public class Segundo {
 		try {
 			in = new FileInputStream("C:\\Users\\aula9\\Desktop\\datos");
 
-//		int i = in.read() * 0x1000000 + in.read() * 0x10000 + in.read() * 0x1000 + in.read();
 			int i = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-			
-			long l = ((long)in.read() << 56) + ((long)in.read() << 48) + ((long)in.read() << 40) + 
-							 ((long)in.read() << 32) + (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
+
+			long l = ((long) in.read() << 56) + ((long) in.read() << 48) + ((long) in.read() << 40)
+					+ ((long) in.read() << 32) + (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
 
 			boolean b1 = in.read() != 0;
 			boolean b2 = in.read() != 0;
-			
+
 			System.out.println(i);
 			System.out.println(l);
 			System.out.println(b1);
 			System.out.println(b2);
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
